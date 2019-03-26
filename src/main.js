ceddc52dc6a816cc47852c303215faa6cf764cf7
haomengshu <haomengshu@zhicall.cn>
2019-03-26 17:33:21 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body) // 解决移动端点击延迟300ms的问题
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+// 路由就是根据网址的不同，返回不同的内容给用户
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader, HomeSwiper
+  }
+}
+</script>
+
+<style>
+</style>

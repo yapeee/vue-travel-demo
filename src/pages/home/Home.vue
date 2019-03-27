eae444d40bfc8726400137b8252adca5c982bd1d
haomengshu <haomengshu@zhicall.cn>
2019-03-27 18:23:04 +0800

    home:icons & recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <div>test</div>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 export default {
   name: 'Home',
   components: {
-    HomeHeader, HomeSwiper
+    HomeHeader, HomeSwiper, HomeIcons, HomeRecommend
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption" v-if="showSwiper">
-      <swiper-slide v-for="item of list" :key="item.id">
-        <img class="swiper-img" :src="item.imgUrl" />
+    <swiper :options="swiperOption">
+      <!--<swiper-slide v-for="item of list" :key="item.id">-->
+        <!--<img class="swiper-img" :src="item.imgUrl" />-->
+      <!--</swiper-slide>-->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -12,21 +15,31 @@
 <script>
 export default {
   name: 'HomeSwiper',
-  props: {
-    list: Array
-  },
+  // props: {
+  //   list: Array
+  // },
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination',
-        loop: true
-      }
+        pagination: '.swiper-pagination', // 分页器
+        loop: true // 循环
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553660858654&di=1d12f0b98490fff08e0c6a826cf61eb9&imgtype=0&src=http%3A%2F%2Fbpic.ooopic.com%2F18%2F51%2F84%2F18518492-41f9b17397805f54d9367e7fbd2c09b1-0.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553660858764&di=5461f0f6c93bf470a330d25d0b38e23d&imgtype=0&src=http%3A%2F%2Fpic.90sjimg.com%2Fback_pic%2F00%2F04%2F20%2F33%2Fc5a18789359b4fa3d5af6dcf3e2ebe74.jpg'
+        }
+      ]
     }
   },
   computed: {
-    showSwiper () {
-      return this.list.length
-    }
+    // showSwiper () {
+    //   return this.list.length
+    // }
   }
 }
 </script>
